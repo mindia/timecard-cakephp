a2676f3e59237643b30749b7f8f20ea7a6e8c8b8
sakai <aak6090@gmail.com>
2013-12-10 08:57:33 +0900

    ruby on rails のDB構成にあわせる

---
+++ app/Config/Migration/1386741475_add_column_providers_table.php
@@ -0,0 +1,54 @@
+<?php
+class AddColumnProvidersTable extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'providers' => array(
+					'foreign_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'after' => 'name'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'providers' => array('foreign_id',),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
